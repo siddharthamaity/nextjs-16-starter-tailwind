d00c612ba02a2034bc106b51912e9fe5fe74ffae
Siddhartha Maity <siddharthamaity1993@gmail.com>
2025-11-16 19:41:12 +0530

    chore: update dependencies and devDependencies in package.json
    - Bump Next.js version from 16.0.1 to 16.0.3
    - Update @next/bundle-analyzer and @next/eslint-plugin-next to 16.0.3
    - Upgrade @tailwindcss/postcss to 4.1.17
    - Update @types/node to 24.10.1, @types/react to 19.2.5, and @types/react-dom to 19.2.3
    - Bump eslint-config-next to 16.0.3
    - Upgrade tailwindcss to 4.1.17
    - Update typescript-eslint to 8.46.4
    
    feat: update Dockerfile version in SetupDetails.tsx
    - Change Dockerfile for Node.js version from 24.11.0 to 24.11.1 and Bun from 1.3.1 to 1.3.2

--- src/app/(delete-this-and-modify-page.tsx)/SetupDetails.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/SetupDetails.tsx
@@ -29,7 +29,7 @@ const FEATURES: Feature[] = [
     { name: 'App Directory' },
     { name: 'System, Light & Dark Mode' },
     { name: 'Next Bundle Analyzer' },
-    { name: 'Dockerfile for Node.js 24.11.0 & Bun 1.3.1 (alpine)' }
+    { name: 'Dockerfile for Node.js 24.11.1 & Bun 1.3.2 (alpine)' }
 ];
 
 const ESLINT_PLUGINS: Plugin[] = [
